14cd98cc3c7e4944d87fd76f89f5e20d31119f82
Kenny W. Lino <kennywlino@gmail.com>
2022-09-19 16:01:48 -0700

    Added basic form and button for adding new ToDo items.

--- src/layout.js
+++ src/layout.js
@@ -2,6 +2,7 @@ import ToDo from "./todo";
 import Project from "./project";
 import './style.css';
 
+// defines header to display page title
 function createHeader() {
     const header = document.createElement("header");
     header.classList.add("header");
@@ -10,29 +11,58 @@ function createHeader() {
     pageName.textContent = "ToDo List";
 
     header.appendChild(pageName);
+    
+    header.appendChild(createAddTodoButton());
 
     return header;
 }
 
+// defines an "Add ToDo" button to include within the header
+// opens a pop-up window for user to add new ToDo on-click
+function createAddTodoButton() {
+    const div = document.createElement("div");
+    div.classList.add("add-todo-div");
+    
+    const button = document.createElement("button");
+    button.classList.add("add-todo");
+    button.textContent = "+";
+
+    // pop-up menu on-click
+    button.addEventListener("click", addTodo);
+
+    div.appendChild(button);
+    
+    return div;
+}
+
+function addTodo() {
+    alert("adding todo");
+}
+
+// defines form to take in user input for ToDo items
+function createTodoForm() {
+    const div = document.createElement("div");
+    
+    const form = document.createElement("form");
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "todo");
+    input.setAttribute("placeholder", "Add a ToDo Item");
+
+    form.appendChild(input);
+
+    div.append(form);
+
+    return div;
+}
+
+// sets up the entire webpage with default view
+// e.g. sidebar closed and only today's TODOs displayed
 function setUpPage() {
     const content = document.getElementById("content");
     content.appendChild(createHeader());
+    content.appendChild(createTodoForm());
 }
-
-/* const testTodo1 = new ToDo("homework");
-const testTodo2 = new ToDo("workout");
-
-console.log(testTodo1.title);
-console.log(testTodo2.title)
-
-const content = document.querySelector('#content');
-
-const todoDiv = document.createElement('div');
-todoDiv.textContent = testTodo1.title;
-todoDiv.classList.add('hello');
-
-content.appendChild(todoDiv);
-
-*/
 
 export default setUpPage;
